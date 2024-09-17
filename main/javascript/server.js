9081032485c1ebf4a525d9d3bd39ac2dfac137e5
blameazu <pig96922@gmail.com>
2024-09-17 23:56:00 +0800

    server.js updates

--- main/javascript/server.js
+++ main/javascript/server.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var con = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
