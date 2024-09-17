84ddd9a02057b7e8be3398b2620474e81d53a0cf
blameazu <pig96922@gmail.com>
2024-09-17 21:34:17 +0800

    small update 2

--- main/javascript/server.js
+++ main/javascript/server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.static(path.join(__dirname, '..'))); 
+app.use(express.static(path.join(__dirname, '..', '..'))); 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,7 +23,7 @@ con.connect(function(err) {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'main.html'));
+  res.sendFile(path.join(__dirname, '..', '..', 'index.html'));
 });
 
 app.get('/login.html', (req, res) => {
